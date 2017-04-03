32ce8a4adb902b70e52402884bc432cb39447286
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-04-03 20:40:41 +0200

    Simple collages route

--- routes/collages.js
+++ routes/collages.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/:id', function(req, res, next) {
+  res.json({
+    id: req.params.id
+  })
 });
 
 module.exports = router;
