ad50310c904ab5d7cce2a6131a695f98add67d94
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-04-06 22:37:37 +0200

    Animating a bar

--- client/src/presentationals/MissingCollage.js
+++ client/src/presentationals/MissingCollage.js
@@ -5,9 +5,17 @@ import './MissingCollage.css';
 const MissingCollage = () => {
   return (
     <div className="MissingCollage">
-      <h1 className="MissingCollage__text">
-        Bybilledet er endnu ikke klar
-      </h1>
+      <div className="MissingCollage__right-bordered">
+        <h1 className="MissingCollage__text">
+          Bybilledet
+        </h1>
+        <h1 className="MissingCollage__text">
+          er endnu
+        </h1>
+        <h1 className="MissingCollage__text">
+          ikke klar
+        </h1>
+      </div>
     </div>
   );
 };
