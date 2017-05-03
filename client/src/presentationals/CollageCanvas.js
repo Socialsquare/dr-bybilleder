cdf404966cf60a974a317921c3c69664fa1fe00a
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-05-03 14:42:07 +0200

    Moved helper functions into seperate file

--- client/src/presentationals/CollageCanvas.js
+++ client/src/presentationals/CollageCanvas.js
@@ -1,87 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import { addSource, generateStyle, fullscreen } from '../utils';
+import Video from './Video';
 
 import './CollageCanvas.css';
-
-const CSS_PREFIXES = {
-  'transform': ['-webkit-transform', '-ms-transform']
-};
-
-const generatePrefixes = attributes => {
-  const result = {};
-  // Loop through the attributes
-  Object.keys(attributes).forEach(key => {
-    const value = attributes[key];
-    // Add the original value to the result
-    result[key] = value;
-    // If prefixes exists
-    if(key in CSS_PREFIXES) {
-      const prefixes = CSS_PREFIXES[key];
-      // Add values for each prefix to the attributes
-      prefixes.forEach(prefix => {
-        result[prefix] = value;
-      });
-    }
-  });
-  return result;
-};
-
-const generateStyle = attributes => {
-  const prefixedAttributes = generatePrefixes(attributes);
-  return Object.keys(prefixedAttributes).map(key => {
-    const value = prefixedAttributes[key];
-    return key + ':' + value + ';';
-  }).join('');
-};
-
-const addSource = (element, url, type) => {
-  const sourceElement = document.createElement('source');
-  sourceElement.setAttribute('src', url);
-  sourceElement.setAttribute('type', type);
-  element.appendChild(sourceElement);
-};
-
-const fullscreen = {
-  is: () => {
-    const fullscreenElement = document.fullscreenElement ||
-                              document.webkitFullscreenElement ||
-                              document.mozFullScreenElement ||
-                              document.msFullscreenElement;
-    return !!fullscreenElement;
-  },
-  request: e => {
-    const req = e.requestFullscreen ||
-                e.msRequestFullscreen ||
-                e.mozRequestFullScreen ||
-                e.webkitRequestFullscreen;
-    if(req) {
-      req.call(e);
-    }
-  },
-  exit: () => {
-    if (document.exitFullscreen) {
-    	document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-    	document.webkitExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-    	document.mozCancelFullScreen();
-    } else if (document.msExitFullscreen) {
-    	document.msExitFullscreen();
-    }
-  },
-  addListener: listener => {
-    document.addEventListener('fullscreenchange', listener);
-    document.addEventListener('webkitfullscreenchange', listener);
-    document.addEventListener('mozfullscreenchange', listener);
-    document.addEventListener('MSFullscreenChange', listener);
-  },
-  removeListener: listener => {
-    document.removeEventListener('fullscreenchange', listener);
-    document.removeEventListener('webkitfullscreenchange', listener);
-    document.removeEventListener('mozfullscreenchange', listener);
-    document.removeEventListener('MSFullscreenChange', listener);
-  }
-};
-
 // import DogGridSvg from '../svgs/dot-grid.svg';
 // import PlaySvg from '../svgs/play.svg';
 import FullscreenSvg from '../svgs/fullscreen.svg';
---
+++ client/src/utils.js
@@ -0,0 +1,82 @@
+// CSS prefixing
+const CSS_PREFIXES = {
+  'transform': ['-webkit-transform', '-ms-transform']
+};
+
+const generatePrefixes = attributes => {
+  const result = {};
+  // Loop through the attributes
+  Object.keys(attributes).forEach(key => {
+    const value = attributes[key];
+    // Add the original value to the result
+    result[key] = value;
+    // If prefixes exists
+    if(key in CSS_PREFIXES) {
+      const prefixes = CSS_PREFIXES[key];
+      // Add values for each prefix to the attributes
+      prefixes.forEach(prefix => {
+        result[prefix] = value;
+      });
+    }
+  });
+  return result;
+};
+
+export const generateStyle = attributes => {
+  const prefixedAttributes = generatePrefixes(attributes);
+  return Object.keys(prefixedAttributes).map(key => {
+    const value = prefixedAttributes[key];
+    return key + ':' + value + ';';
+  }).join('');
+};
+
+
+// Helper functions
+export const addSource = (element, url, type) => {
+  const sourceElement = document.createElement('source');
+  sourceElement.setAttribute('src', url);
+  sourceElement.setAttribute('type', type);
+  element.appendChild(sourceElement);
+};
+
+export const fullscreen = {
+  is: () => {
+    const fullscreenElement = document.fullscreenElement ||
+                              document.webkitFullscreenElement ||
+                              document.mozFullScreenElement ||
+                              document.msFullscreenElement;
+    return !!fullscreenElement;
+  },
+  request: e => {
+    const req = e.requestFullscreen ||
+                e.msRequestFullscreen ||
+                e.mozRequestFullScreen ||
+                e.webkitRequestFullscreen;
+    if(req) {
+      req.call(e);
+    }
+  },
+  exit: () => {
+    if (document.exitFullscreen) {
+    	document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+    	document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+    	document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+    	document.msExitFullscreen();
+    }
+  },
+  addListener: listener => {
+    document.addEventListener('fullscreenchange', listener);
+    document.addEventListener('webkitfullscreenchange', listener);
+    document.addEventListener('mozfullscreenchange', listener);
+    document.addEventListener('MSFullscreenChange', listener);
+  },
+  removeListener: listener => {
+    document.removeEventListener('fullscreenchange', listener);
+    document.removeEventListener('webkitfullscreenchange', listener);
+    document.removeEventListener('mozfullscreenchange', listener);
+    document.removeEventListener('MSFullscreenChange', listener);
+  }
+};
