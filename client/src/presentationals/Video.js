eda9baa218c7aad3d9301c5df923a45185b8c667
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-05-04 10:29:18 +0200

    Migrated to prop-types package

--- client/src/presentationals/Video.js
+++ client/src/presentationals/Video.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { generateStyle } from '../utils';
 
 export default class Video extends Component {
